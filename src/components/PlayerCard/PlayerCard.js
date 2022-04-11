c51c9026328a8742d8307f471d25db6f0250b852
cantutar <vahitcant@gmail.com>
2022-04-11 17:49:30 +0300

    fixed a little issue on playercard

--- src/components/PlayerCard/PlayerCard.js
+++ src/components/PlayerCard/PlayerCard.js
@@ -5,13 +5,16 @@ import { useState } from "react";
 const testImageUrl =
   "https://media.valorant-api.com/playercards/33c1f011-4eca-068c-9751-f68c788b2eee/largeart.png";
 export default function PlayerCard(props) {
-  const [playerName, setPlayerName] = useState("NEKROFİLİKAISER");
   return (
     <>
       <div className="d-flex justify-content-center">
-        <h4 className={`text-white ${classes.nick}`}>{playerName}</h4>
-        <h6 className={`text-white ${classes.title}`}>{playerName}</h6>
-        <img className={`rounded ${classes.image}`} src={testImageUrl} alt="" />
+        <h4 className={`text-white ${classes.nick}`}>{props.playerName}</h4>
+        <h6 className={`text-white ${classes.title}`}>{props.playerName}</h6>
+        <img
+          className={`rounded ${classes.image}`}
+          src={props.testImageUrl}
+          alt=""
+        />
         <img src={overlay} alt="" className={`${classes.overlay}`} />
       </div>
     </>
